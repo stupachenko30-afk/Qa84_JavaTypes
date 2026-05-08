6fcff065ba78c44b37fa416203868f61df08c21b
Yevhenii <stupachenko30@gmail.com>
2026-05-08 10:29:08 +0200

    add1

---
+++ JavaTypes1/src/main/java/MainJavaTypes.java
@@ -0,0 +1,39 @@
+public class MainJavaTypes {
+    public static void main (String[] args){
+        System.out.println("Hello");
+        // primitive type
+        //  целые числа
+        int a = 5;// a->[][][][] -4 байта
+        byte b = 127;//b->[]
+        short c = 34; //c->[][]
+        char n = '#'; //n->[][] ascii-стандарт кодировки символов
+        long m = 2149000000L; //[][][][][][][][] -8 байта
+
+        //дробные числа
+        double t = 34.8; // t->[][][][][][][][]
+        float g = 3.8F;//g->[][][][]
+
+        boolean x = true; // x->[]
+
+        //reference type -ссылочный тип
+        String s = "Hello Java";
+
+        int k = 8;
+        int h = k;
+        /*
+        k->[][][][8]
+        h->[][][][8]
+         */
+           String str = "Hello";
+           String str1 = str;
+           String str2 = str1;
+        System.out.println(str2);
+
+           /*
+           str->["Hello"]
+           str1->["Hello"]
+           str2->["Hello"]
+            */
+
+    }
+}
